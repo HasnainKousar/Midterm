1e735609ce05c8edc0280753a8e500be4e97270a
Hasnain <kousar@DellKousar.localdomain>
2025-06-27 14:39:40 -0400

    created the factory class to register and create operations instances, and added doc string

--- app/operations.py
+++ app/operations.py
@@ -2,8 +2,24 @@
 # Operations file for the application
 ###################################
 
-""""
+"""
+This module defines various mathematical operations as classes.
+Each operation class inherits from the abstract base class `Operation`.
+It includes methods for executing the operation and validating operands.
 
+We will implement the following operations:
+- Addition
+- Subtraction
+- Multiplication
+- Division
+- Power
+- Root
+- Modulus
+- Integer Division
+- Percentage
+- Absolute Difference
+
+we will use the factory pattern to create instances of these operations.
 
 """
 
@@ -23,18 +39,33 @@ class Operation(ABC):
         """
         Execute the operation with two decimal numbers.
         
-        :param a: First number
-        :param b: Second number
-        :return: Result of the operation
+        Perform the operation defined by the subclass on the two operands.
+
+        Args: 
+            a (Decimal): First operand.
+            b (Decimal): Second operand.
+
+        Returns:
+            Decimal: Result of the operation.
         """
         pass
 
     def validate_operands(self, a: Decimal, b: Decimal) -> None:
+        """
+        Validate the operands for the operation.
+
+        This method checks if the operands are valid for the operation.
+        It can be overridden by subclasses to implement specific validation rules.
+
+        Args:
+            a (Decimal): First operand.
+            b (Decimal): Second operand.
+
+
+        Raises:
+            ValidationError: If the operands are not valid for the operation.
         
-
-
-
-
+        """
         pass
 
 
@@ -49,11 +80,11 @@ class Operation(ABC):
 
 
 class Addition(Operation):
-
-
-
+    """
+    Class for addition operation.
     
-
+    Performs addition of two decimal numbers.
+    """
     def execute(self, a: Decimal, b: Decimal) -> Decimal:
         """
         Execute the addition operation.
@@ -67,6 +98,11 @@ class Addition(Operation):
     
 
 class Subtraction(Operation):
+    """
+    Class for subtraction operation.
+
+    Performs subtraction of two decimal numbers.
+    """
 
     def execute(self, a: Decimal, b: Decimal) -> Decimal:
         """
@@ -82,6 +118,12 @@ class Subtraction(Operation):
 
 
 class Multiplication(Operation):
+    """
+    Class for multiplication operation.
+
+
+    Performs multiplication of two decimal numbers.
+    """
 
     def execute(self, a: Decimal, b: Decimal) -> Decimal:
         """
@@ -96,11 +138,22 @@ class Multiplication(Operation):
     
 
 class Division(Operation):
+    """
+    Class for division operation.
+
+    Performs division of two decimal numbers.
+    """
 
     def validate_operands(self, a, b):
-
-
-
+        """
+        Validate the operands for the division operation.
+        This method checks if the second operand is zero, which would cause a division by zero error.
+        
+        We override the base class method to add specific validation for division.
+        :param a: First number
+        :param b: Second number
+        :raises ValidationError: If the second operand is zero.
+        """
         super().validate_operands(a, b)
         if b == 0:
             raise ValidationError("Division by zero is not allowed.")
@@ -119,11 +172,23 @@ class Division(Operation):
     
 
 class power(Operation):
+    """
+    Class for power operation.
+
+    Performs exponentiation of a base number raised to a power.
+    """
 
     def validate_operands(self, a, b):
+        """
+        Validate the operands for the power operation.
+        This method checks if the second operand (exponent) is negative, which is not allowed
+        for this operation. 
         
-        
-        
+        We override the base class method to add specific validation for power.
+        :param a: Base number
+        :param b: Exponent
+        :raises ValidationError: If the exponent is negative.
+        """
         super().validate_operands(a, b)
 
         if b < 0:
@@ -142,8 +207,24 @@ class power(Operation):
         return Decimal(pow(float(a), float(b)))
     
 class Root(Operation):
+    """Class for root operation.
+
+    Performs the calculation of the nth root of a number.
+    """
+
 
     def validate_operands(self, a, b):
+        """
+        Validate the operands for the root operation.
+        This method checks if the first operand (number) is negative and the second
+        operand (degree) is less than or equal to zero, which is not allowed for this operation.
+
+        We override the base class method to add specific validation for root.
+        :param a: Number to find the root of
+        :param b: Degree of the root
+        :raises ValidationError: If the number is negative or the degree is less than or equal to zero.
+        """
+
         super().validate_operands(a, b)
         if a < 0:
             raise ValidationError("Cannot calculate the root of a negative number.")
@@ -163,8 +244,22 @@ class Root(Operation):
     
 
 class Modulus(Operation):
+    """Class for modulus operation. 
+
+    Performs the modulus operation, which returns the remainder of the division of two numbers.
+    """
 
     def validate_operands(self, a, b):
+        """
+        Validate the operands for the modulus operation.
+        This method checks if the second operand (modulus) is zero, which would cause a
+        modulus by zero error.
+
+        We override the base class method to add specific validation for modulus.
+        :param a: First number  
+        :param b: Second number (modulus)
+        :raises ValidationError: If the second operand is zero.
+        """
         
         super().validate_operands(a, b)
         if b == 0:
@@ -182,8 +277,24 @@ class Modulus(Operation):
         return a % b
     
 class IntegerDivision(Operation):
+    """Class for integer division operation.
+
+    Performs integer division of two decimal numbers, returning the quotient without the remainder.
+    """
 
     def validate_operands(self, a, b):
+        """
+        Validate the operands for the integer division operation.
+        This method checks if the second operand (divisor) is zero, which would cause a
+        division by zero error, and if both operands are integers.  
+
+        We override the base class method to add specific validation for integer division.
+        :param a: First number (dividend)
+        :param b: Second number (divisor)
+        :raises ValidationError: If the second operand is zero or if either operand is not an integer.
+        """
+
+
         super().validate_operands(a, b)
         if b == 0:
             raise ValidationError("Integer division by zero is not allowed.")
@@ -202,8 +313,10 @@ class IntegerDivision(Operation):
         return a // b
     
 class Percentage(Operation):
-    """ class for percentage operation.
-    
+    """ Class for percentage operation.
+
+    Performs the calculation of a percentage of a number.
+
     """
         
     def execute(self, a: Decimal, b: Decimal) -> Decimal:
@@ -217,6 +330,101 @@ class Percentage(Operation):
         self.validate_operands(a, b)
         return (a * b) / 100
 
-   
+
+class AbsoluteDifference(Operation):
+    """ Class for absolute difference operation.
+
+    Performs the calculation of the absolute difference between two numbers.
+    """
+    
+    def execute(self, a: Decimal, b: Decimal) -> Decimal:
+        """
+        Execute the absolute difference operation.
+        
+        :param a: First number
+        :param b: Second number
+        :return: Result of the absolute difference operation
+        """
+        self.validate_operands(a, b)
+        return abs(a - b)
+    
+
+
+
+
+class OperationFactory:
+    """
+    Factory class to create operation instances.
+
+    This class provides a way to create instances of various mathematical operations
+    based on the operation type. It uses a dictionary to map operation names to their
+    corresponding classes. New operations can be registered dynamically.
+
+    Attributes:
+        _operations (Dict[str, type]): A dictionary mapping operation names to their classes.
+
+    """
+
+    _operations: Dict[str, type] = {
+        "add": Addition,
+        "subtract": Subtraction,
+        "multiply": Multiplication,
+        "divide": Division,
+        "power": power,
+        "root": Root,
+        "modulus": Modulus,
+        "integerdivide": IntegerDivision,
+        "percentage": Percentage,
+        "absolute": AbsoluteDifference
+    }   
+
+    @classmethod
+    def register_operation(cls, name: str, operation_class: type) -> None:
+        """
+        Register a new operation class with a name.
+
+        args:
+            name (str): the name of the operation (e.g., "add", "subtract", etc.).
+            operation_class (type): the class implementing the operation, which must be a subclass of Operation
+
+        Raises:
+            TypeError: If the operation_class is not a subclass of Operation.
+
+        """
+
+
+        if not issubclass(operation_class, Operation):
+            raise TypeError("Operation class must be a subclass of Operation.")
+        cls._operations[name.lower()] = operation_class
+
+
+    @classmethod
+    def create_operation(cls, operation_type: str) -> Operation:
+        """
+        Create an instance of the specified operation type.
+
+        This method retrieves the operation class from the _operation dictionary
+        and creates an instance of it.
+
+        Args:
+            operation_type (str): The type of operation to create (e.g., "add",
+            "subtract", "multiply", etc.).
+
+        Returns:
+            Operation: An instance of the specified operation class.
+
+        Raises:
+            ValueError: If the operation type is not recognized.
+        """
+
+        operation_class = cls._operations.get(operation_type.lower())
+        if not operation_class:
+            raise ValueError(f"Unkown operation type: {operation_type}")
+        return operation_class()
+    
+
+
+
+
 
     
