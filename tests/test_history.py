78e9e1e75fd8bb8a8c9309cc5a0bf9a917c80191
Hasnain <kousar@DellKousar.localdomain>
2025-06-29 10:50:10 -0400

    added test for LoggingObserver

--- tests/test_history.py
+++ tests/test_history.py
@@ -23,3 +23,22 @@ calculation_mock.operand2 = 2
 calculation_mock.result = 8
 
 
+#######################
+# Test LoggingObserver
+#######################
+
+@patch('logging.info')
+def test_logging_observer_logs_calculation(mock_logging_info):
+    """Test that LoggingObserver logs the calculation correctly."""
+    observer = LoggingObserver()
+    observer.update(calculation_mock)
+    logging.info.assert_called_once_with(
+        f"Calculation performed: addition (6, 2) = 8"
+    )
+
+def test_logging_observer_none_calculation():
+    """Test LoggingObserver raises AttributeError when calculation is None."""
+    observer = LoggingObserver()
+    with pytest.raises(AttributeError):
+        observer.update(None)
+
