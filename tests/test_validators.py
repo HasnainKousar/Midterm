5cc438b40ef547b704dbad851c85278473f472f2
Hasnain <kousar@DellKousar.localdomain>
2025-06-28 23:44:37 -0400

    completed positive test cases for test_validators.py

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -16,6 +16,9 @@ from app.input_validators import InputValidator
 config = CalculatorConfig(max_input_value=Decimal('100000'))
 
 
+############################
+# Positive Test Cases
+############################
 def test_validate_number_positive_integer():
     """Test validation of a positive integer."""
     assert InputValidator.validate_number(42, config) == Decimal('42')
@@ -31,4 +34,37 @@ def test_validate_positive_decimal():
 def test_validate_negative_decimal():
     """Test validation of a negative decimal."""
     assert InputValidator.validate_number(Decimal('-2.718'), config) == Decimal('-2.718')
+
+def test_validate_number_positive_string():
+    """Test validation of a positive number in string format."""
+    assert InputValidator.validate_number('154', config) == Decimal('154')
+
+def test_validate_number_postive_decimal_string():
+    """Test validation of a positive decimal in string format."""
+    assert InputValidator.validate_number('77.123', config) == Decimal('77.123')
+
+def test_validate_number_negative_string():
+    """Test validation of a negative number in string format."""
+    assert InputValidator.validate_number('-42', config) == Decimal('-42')
+
+def test_validate_number_negative_decimal_string():
+    """Test validation of a negative decimal in string format."""
+    assert InputValidator.validate_number('-3.14', config) == Decimal('-3.14')
+
+def test_validate_number_zero():
+    """Test validation of zero."""
+    assert InputValidator.validate_number(0, config) == Decimal('0')
+
+def test_validate_number_trimmed_string():
+    """Test validation of a number with leading/trailing spaces."""
+    assert InputValidator.validate_number('   245.231   ', config) == Decimal('245.231')
+
+#############################
+# Negative Test Cases
+#############################
+
+
+
+
+
 
