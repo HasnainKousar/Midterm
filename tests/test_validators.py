7e4f9197878322943b83984ee421deecf09ef2d7
Hasnain <kousar@DellKousar.localdomain>
2025-06-28 23:40:41 -0400

    added few tests to test_validators.py

---
+++ tests/test_validators.py
@@ -0,0 +1,34 @@
+###########################
+# test_validators.py
+###########################
+
+"""
+
+"""
+from logging import config
+import pytest
+from decimal import Decimal
+from app.calculator_config import CalculatorConfig
+from app.exceptions import ValidationError
+from app.input_validators import InputValidator
+
+# sample configuration for testing with max input value of 100,000
+config = CalculatorConfig(max_input_value=Decimal('100000'))
+
+
+def test_validate_number_positive_integer():
+    """Test validation of a positive integer."""
+    assert InputValidator.validate_number(42, config) == Decimal('42')
+
+def test_validate_number_negative_integer():
+    """Test validation of a negative integer."""
+    assert InputValidator.validate_number(-18, config) == Decimal('-18')
+
+def test_validate_positive_decimal():
+    """Test validation of a positive decimal."""
+    assert InputValidator.validate_number(Decimal('3.14'), config) == Decimal('3.14')
+
+def test_validate_negative_decimal():
+    """Test validation of a negative decimal."""
+    assert InputValidator.validate_number(Decimal('-2.718'), config) == Decimal('-2.718')
+
