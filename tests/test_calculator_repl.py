e63cb7bd5b203b2d878e7b88f398c40fcb2b9f24
Hasnain <kousar@DellKousar.localdomain>
2025-06-29 20:25:07 -0400

    added test for valid exit command

---
+++ tests/test_calculator_repl.py
@@ -0,0 +1,26 @@
+#############################
+# Tests for the Calculator REPL
+#############################
+
+"""
+
+"""
+
+from unittest import mock
+import pytest
+from unittest.mock import Mock, patch
+from app.calculator_repl import start_calculator_repl
+from app.exceptions import OperationError, ValidationError
+
+
+#Test case for exitting the REPL with a valid command
+@patch('builtins.input', side_effect=['exit'])
+@patch('builtins.print')
+def test_run_calculator_repl_exit(mock_print, mock_input):
+    with patch('app.calculator.Calculator.save_history') as mock_save_history:
+        start_calculator_repl()
+        mock_save_history.assert_called_once()
+        mock_print.assert_any_call("History saved successfully.")
+        mock_print.assert_any_call("Exiting calculator REPL. Goodbye!")
+
+
